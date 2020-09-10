88e25c39b327d5cc277834e7acd42dd02f09fc68
Valera <valera.krivorot@gmail.com>
2020-09-10 18:55:24 +0300

    1

---
+++ src/test.py
@@ -0,0 +1,19 @@
+
+
+def func(a: int, b: int) -> int:
+    """Summ 2 int values
+
+    :param a: first digint
+    :type a: int
+    :param b: second digit
+    :type b: int
+    :return: summ
+    :rtype: int
+    """    
+    return a + b
+
+
+func(2, 3)
+
+
+# decorator 
